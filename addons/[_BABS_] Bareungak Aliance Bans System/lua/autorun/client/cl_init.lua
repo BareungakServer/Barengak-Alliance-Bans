15cba25127a1959037fa7ce3e0899ee98adacce1
Bareungak <tjdwlself6@gmail.com>
2018-01-19 22:05:28 +0900

    Bareungak Alliance Bans
    Addon 1.0 File

---
+++ addons/[_BABS_] Bareungak Aliance Bans System/lua/autorun/client/cl_init.lua
@@ -0,0 +1,33 @@
+﻿if CLIENT then
+
+net.Receive( "AliBanMenu", function( len, ply )
+if(LocalPlayer():IsSuperAdmin()) then
+local Frame = vgui.Create( "DFrame" )
+Frame:SetSize( 300, 160 )
+Frame:SetTitle( "바른각 연합밴 시스탬 (v1.0)" )
+Frame:SetVisible( true )
+Frame:SetDraggable( true )
+Frame:ShowCloseButton( true )
+Frame:MakePopup()
+	
+local DermaButton = vgui.Create( "DButton", Frame )
+DermaButton:SetText( "연합밴 목록 업데이트" )
+DermaButton:SetPos( 25, 50 )
+DermaButton:SetSize( 250, 30 )
+DermaButton.DoClick = function()	
+	RunConsoleCommand("aliban_update")
+end
+
+local DermaButton = vgui.Create( "DButton", Frame )
+DermaButton:SetText( "연합밴 홈페이지" )
+DermaButton:SetPos( 25, 100 )
+DermaButton:SetSize( 250, 30 )
+DermaButton.DoClick = function()	
+	gui.OpenURL( "http://barncs.xyz/Barn_Server/BarnASV/" )
+end
+
+else
+return false
+end
+end)
+end
---
+++ addons/[_BABS_] Bareungak Aliance Bans System/lua/autorun/server/sv_init.lua
@@ -0,0 +1,90 @@
+﻿--[[
+바른각 연합밴 시스탬 (BABS) v1.0
+ⓒ 2016 ~ 2018 Breaking Studio, Bareungak Alliance Bans 모든 권리 보유.
+]]
+
+AliBansConfig = {}
+local Config = AliBansConfig
+Config.Version = "1.0"
+Config.Commander = "!연합밴"
+
+if SERVER then
+util.AddNetworkString( "AliBanMenu" )
+    print("[BABS] 바른각 연합밴 시스탬 로드....")
+	banlist = {}
+	content = ""
+	local url = "http://119.199.73.181/Barn_Server/db_bans.txt"
+	
+	function check()
+	http.Fetch(url, function(html)
+			banlist = string.Explode("|",html)
+			content = html
+			if(banlist[1] > Config.Version) then
+			print("[BABS] 바른각 연합밴 시스탬 업데이트가 필요합니다!")
+
+			for k, v in pairs(player.GetAll()) do
+				v:PrintMessage( HUD_PRINTTALK, "[BABS] 새로운 업데이트가 있습니다!" )
+			end
+
+			end
+			
+			for k, v in pairs(player.GetAll()) do
+				v:PrintMessage( HUD_PRINTTALK, "[BABS] 연합밴 데이터베이스 불러오기 성공!" )
+			end
+		    print("[BABS] 연합밴 데이터베이스 목록을 불러왔습니다.")
+	end)
+	end
+	
+	function usercheck()
+			for k, v in pairs(player.GetAll()) do
+				for _,ban in pairs(banlist) do
+					if ban == v:SteamID() then
+					    kickuser(v)
+					end
+				end
+			end
+	end
+	
+	function aliget(v)
+	usercheck()
+				for _,ban in pairs(banlist) do
+					if ban == v:SteamID() then
+					     kickuser(v)
+					end
+				end
+	end
+	
+             function kickuser(v)
+             	 print("Kick " .. v:Nick() .. " by BABS")
+             	v:Kick("[BABS] 당신은 바른각 연합밴 시스탬에 의하여 추방됬습니다.\n연합밴 사유: http://barncs.xyz/Barn_Server/BarnASV/")
+             	for k, v in pairs(player.GetAll()) do
+				v:PrintMessage( HUD_PRINTTALK, v:Nick() .. " 님이 바른각 연합밴 시스탬에 의해 차단당했습니다!")
+		end
+             end
+
+	function init()
+	check()
+	timer.Create("UpdateAlibanList", 120 , 0, check)
+	print("[BABS] 바른각 연합밴 시스탬을 구성중 입니다...")
+	end
+	
+	hook.Add( "PlayerInitialSpawn", "CheckUsers", aliget )
+	hook.Add( "Initialize", "InitializeAliBans", init )
+	
+	concommand.Add( "aliban_update", function( ply, cmd, args )
+	check()
+end )
+
+
+	hook.Add( "PlayerSay", "AliBanMenu", function( ply, text, public )
+	if ( text == Config.Commander ) then
+	if(ply:IsSuperAdmin()) then
+	net.Start( "AliBanMenu" )
+	net.WriteString(content)
+    net.Send(ply)
+	else
+	return false
+	end
+	end
+	end)
+end
